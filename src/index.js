5ce61b8d7f263d3ea7b082c54c53393e1c69613c
Kristofer Myhre <kristofer.myhre@acando.no>
2019-04-24 20:33:59 +0200

    updated exports

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 
-import {getBootstrap} from './requests/getBootstrap';
-import {getGameweeks} from './requests/getGameweeks';
-import {getLeague} from './requests/getLeague';
-import {getPlayer} from './requests/getPlayer';
-import {getPLTeams} from './requests/getPLTeams';
-import {getTeam} from './requests/getTeam';
-import {getTeamByGW} from './requests/getTeamByGW';
+import getBootstrap from './requests/getBootstrap';
+import getGameweeks from './requests/getGameweeks';
+import getLeague from './requests/getLeague';
+import getPlayer from './requests/getPlayer';
+import getPLTeams from './requests/getPLTeams';
+import getTeam from './requests/getTeam';
+import getTeamByGW from './requests/getTeamByGW';
 
 module.exports = {
     getBootstrap,
